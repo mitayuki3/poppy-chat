dbf182e89c1373b1209a6bd01e594d71014f955c
Yuki MITA <mitayuki3@yahoo.co.jp>
2025-05-15 00:42:57 +0900

    テキストボックスを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import TextField from "@mui/material/TextField";
+
 export default function Home() {
 	return (
 		<main>
+			<TextField
+				label="システムプロンプト"
+				variant="outlined"
+				fullWidth
+				margin="normal"
+				multiline
+				minRows={3}
+			/>
 			<div>Hello world!</div>
 		</main>
 	);
