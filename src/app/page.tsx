76c850657aa42cb0d8b80a3042773f91e97226c8
Yuki MITA <mitayuki3@yahoo.co.jp>
2025-05-18 21:38:21 +0900

    適当なテキストフィールド

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
 
 export default function Home() {
 	return (
 		<main>
-			<TextField
-				label="システムプロンプト"
-				variant="outlined"
-				fullWidth
-				margin="normal"
-				multiline
-				minRows={3}
-			/>
-			<div>Hello world!</div>
+			<Stack spacing={1}>
+				<TextField
+					label="システムプロンプト"
+					variant="outlined"
+					fullWidth
+					multiline
+				/>
+				<TextField multiline defaultValue="Hello world!" />
+				<TextField
+					fullWidth
+					label="新しいメッセージ (Ctrl+Enter)"
+					variant="outlined"
+				/>
+			</Stack>
 		</main>
 	);
 }
